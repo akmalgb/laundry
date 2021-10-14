8154b6f96417b1f14ea4f2e23d80f0efdfdac400
Akmal Gibran Baihaqi <akmalbaihaqigibran@gmail.com>
2021-10-14 15:31:33 +0700

    +configure server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+//call member's router
+const member = require("./routers/member");
+
+app.use("/member", member)
+//call member's router
+
 app.listen(8000, () => {
-    console.log('aku berlari kencang di port 8000');
+    console.log('Server berlari kencang di port 8000');
 })
