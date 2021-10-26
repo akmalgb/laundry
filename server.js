5f93ee898242aab5942064fca2e6089be9c43a13
Akmal Gibran Baihaqi <akmalbaihaqigibran@gmail.com>
2021-10-26 08:14:49 +0700

    +call routers

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 
-//call member's router
+//call routers
 const member = require("./routers/member");
+const package = require("./routers/package");
+const user = require("./routers/user");
 
-app.use("/member", member)
 //call member's router
+app.use("/member", member)
+
+//call package's router
+app.use("/package", package)
+
+//call user's router
+app.use("/user", user)
 
 app.listen(8000, () => {
     console.log('Server berlari kencang di port 8000');
