7948d2ccd0d28b0eee482f43bd0c30606aa8e8fa
Akmal Gibran Baihaqi <akmalbaihaqigibran@gmail.com>
2021-11-09 09:59:36 +0700

    +create login endpoint for authorization

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const member = require("./routers/member");
 const package = require("./routers/package");
 const user = require("./routers/user");
 const transaction = require("./routers/transaction");
+const { login } = require("./routers/login");
 
 //call member's router
 app.use("/member", member)
@@ -18,6 +19,9 @@ app.use("/user", user)
 
 //call transaction's router
 app.use("/transaction", transaction)
+
+//call login's router
+app.use("/auth", login)
 
 app.listen(8000, () => {
     console.log('Server berlari kencang di port 8000');
